e831cb8c9f533ce9a6ac1cd4f1efc498f3699581
Paul Gibbs <djpaul@gmail.com>
2016-04-28 16:20:59 +1000

    Start stubbing out the Activity endpoints

--- includes/bp-activity/classes/class-bp-activity-endpoints.php
+++ includes/bp-activity/classes/class-bp-activity-endpoints.php
@@ -1,1 +1,337 @@
 <?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Activity endpoints.
+ *
+ * @since 0.1.0
+ */
+class BP_REST_Activity_Controller extends WP_REST_Controller {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.1.0
+	 */
+	public function __construct() {
+		$this->namespace = 'buddypress/v1';
+		$this->rest_base = buddypress()->activity->id;
+	}
+
+	/**
+	 * Register the plugin routes.
+	 *
+	 * @since 0.1.0
+	 */
+	public function register_routes() {
+		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this, 'get_items' ),
+				'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				'args'                => $this->get_collection_params(),
+			),
+			'schema' => array( $this, 'get_public_item_schema' ),
+		) );
+
+		//register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
+		//) );
+	}
+
+	/**
+	 * Get the plugin schema, conforming to JSON Schema.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'activity',
+			'type'       => 'object',
+
+			'properties' => array(
+				'id' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'A unique alphanumeric ID for the object.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'integer',
+				),
+
+				'prime_association' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The ID of some other object primarily associated with this one.', 'buddypress' ),
+					'type'        => 'integer',
+				),
+
+				'secondary_association' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The ID of some other object also associated with this one.', 'buddypress' ),
+					'type'        => 'integer',
+				),
+
+				'author' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The ID for the creator of the object.', 'buddypress' ),
+					'type'        => 'integer',
+				),
+
+				'link' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The permalink to this object on the site.', 'buddypress' ),
+					'format'      => 'url',
+					'type'        => 'string',
+				),
+
+				'component' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The BuddyPress component the object relates to.', 'buddypress' ),
+					'type'        => 'string',
+					'enum'        => array_keys( bp_core_admin_get_components() ),
+				),
+
+				'type' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The activity type of the object.', 'buddypress' ),
+					'type'        => 'string',
+					'enum'        => array_keys( bp_activity_get_types() ),
+				),
+
+				'title' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The title of the object.', 'buddypress' ),
+					'type'        => 'object',
+					'properties'  => array(
+						'raw' => array(
+							'description' => __( 'The title of the object, as it exists in the database.', 'buddypress' ),
+							'type'        => 'string',
+							'context'     => array( 'edit' ),
+						),
+						'rendered' => array(
+							'description' => __( 'HTML title for the object, transformed for display.', 'buddypress' ),
+							'type'        => 'string',
+							'context'     => array( 'view', 'edit' ),
+						),
+					),
+				),
+
+				'content' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The content of the object.', 'buddypress' ),
+					'type'        => 'object',
+					'properties'  => array(
+						'raw' => array(
+							'description' => __( 'The content of the object, as it exists in the database.', 'buddypress' ),
+							'type'        => 'string',
+							'context'     => array( 'edit' ),
+						),
+						'rendered' => array(
+							'description' => __( 'HTML content for the object, transformed for display.', 'buddypress' ),
+							'type'        => 'string',
+							'context'     => array( 'view', 'edit' ),
+						),
+					),
+				),
+
+				'date' => array(
+					'description' => __( "The date the object was published, in the site's timezone.", 'buddypress' ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+					'context'     => array( 'view', 'edit' ),
+				),
+
+				'status' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Whether the object has been marked as spam or not.', 'buddypress' ),
+					'type'        => 'string',
+					'enum'        => array( 'published', 'spam' ),
+				),
+
+				'parent' => array(
+					'description'  => __( 'The ID of the parent of the object.', 'buddypress' ),
+					'type'         => 'integer',
+					'context'      => array( 'view', 'edit' ),
+				),
+			)
+		);
+
+		return $schema;
+	}
+
+	/**
+	 * Get the query params for collections of plugins.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	public function get_collection_params() {
+		$params = parent::get_collection_params();
+
+		$params['context']['default'] = 'view';
+
+		$params['after'] = array(
+			'description'        => __( 'Limit response to resources published after a given ISO8601 compliant date.' ),
+			'type'               => 'string',
+			'format'             => 'date-time',
+			'validate_callback'  => 'rest_validate_request_arg',
+		);
+
+		$params['exclude'] = array(
+			'description'        => __( 'Ensure result set excludes specific ids.' ),
+			'type'               => 'array',
+			'default'            => array(),
+			'sanitize_callback'  => 'wp_parse_id_list',
+		);
+
+		$params['offset'] = array(
+			'description'        => __( 'Offset the result set by a specific number of items.' ),
+			'type'               => 'integer',
+			'sanitize_callback'  => 'absint',
+			'validate_callback'  => 'rest_validate_request_arg',
+		);
+
+		$params['order'] = array(
+			'description'        => __( 'Order sort attribute ascending or descending.' ),
+			'type'               => 'string',
+			'default'            => 'desc',
+			'enum'               => array( 'asc', 'desc' ),
+			'validate_callback'  => 'rest_validate_request_arg',
+		);
+
+		$params['orderby'] = array(
+			'description'        => __( 'Sort collection by object attribute.' ),
+			'type'               => 'string',
+			'default'            => 'date',
+			'enum'               => array(
+				'date',
+				'id',
+			),
+			'validate_callback'  => 'rest_validate_request_arg',
+		);
+
+		$params['parent'] = array(
+			'description'       => __( 'Limit result set to those of particular parent ids.' ),
+			'type'              => 'array',
+			'sanitize_callback' => 'wp_parse_id_list',
+			'default'           => array(),
+		);
+
+		return $params;
+	}
+
+	/**
+	 * Retrieve activities.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param WP_REST_Request $request
+	 * @return WP_REST_Request List of plugin object data.
+	 */
+	public function get_items( $request ) {
+	}
+
+	/**
+	 * Retrieve activity.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param WP_REST_Request $request
+	 * @return WP_REST_Request|WP_Error Plugin object data on success, WP_Error otherwise.
+	 */
+	public function get_item( $request ) {
+	}
+
+	/**
+	 * Check if a given request has access to get information about a specific activity.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return bool
+	 */
+	public function get_item_permissions_check( $request ) {
+		return $this->get_items_permissions_check( $request );
+	}
+
+	/**
+	 * Check if a given request has access to activity items.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|bool
+	 */
+	public function get_items_permissions_check( $request ) {
+		// TODO: handle private activities etc
+		return true;
+	}
+
+	/**
+	 * Prepares activity data for return as an object.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param stdClass $activity Activity data.
+	 * @param WP_REST_Request $request
+	 * @param boolean $is_raw Optional, not used. Defaults to false.
+	 * @return WP_REST_Response
+	 */
+	public function prepare_item_for_response( $activity, $request, $is_raw = false ) {
+		$data = array(
+		);
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		$response = rest_ensure_response( $data );
+		$response->add_links( $this->prepare_links( $activity ) );
+
+		/**
+		 * Filter a plugin value returned from the API.
+		 *
+		 * Allows modification of the plugin value right before it is returned.
+		 *
+		 * @param array           $response
+		 * @param WP_REST_Request $request  Request used to generate the response.
+		 */
+		return apply_filters( 'rest_prepare_buddypress_activity_value', $response, $request );
+	}
+
+	/**
+	 * Prepare links for the request.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $activity Activity.
+	 * @return array Links for the given plugin.
+	 */
+	protected function prepare_links( $activity ) {
+		$base = sprintf( '/%s/%s/', $this->namespace, $this->rest_base );
+
+		// Entity meta.
+		$links = array(
+			'self' => array(
+				'href'   => rest_url( $base . wp_strip_all_tags( $post['id'] ) ),
+			),
+			'collection' => array(
+				'href'   => rest_url( $base ),
+			),
+			'author' = array(
+				'href'       => rest_url( '/wp/v2/users/' . $post->post_author ),
+				'embeddable' => true,
+			)
+		);
+
+		if ( $post_type_obj->hierarchical && ! empty( $post->post_parent ) ) {
+			$links['up'] = array(
+				'href'       => rest_url( trailingslashit( $base ) . (int) $post->post_parent ),
+				'embeddable' => true,
+			);
+		}
+
+		return $links;
+	}
+}
