923a412e63a0448c5d715433bc570f506a9932d4
Renato Alves <espellcaste@gmail.com>
2017-12-08 08:52:07 -0300

    Updating readme files

--- bp-rest.php
+++ bp-rest.php
@@ -11,7 +11,8 @@
  * Requires at least: 4.7.0
  * Tested up to: 4.9.1
  * Requires PHP: 5.5
- * License: GPLv2 or later
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /**
