bbf1c1e266be76a516d80a46579bc9b927726940
Renato Alves <espellcaste@gmail.com>
2018-02-09 11:47:38 -0300

    Activity: Fixing PHPUnit bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,42 +5,30 @@
  * @package BP_REST
  */
 
-/**
- * Determine where the WP test suite lives.
- *
- * Support for:
- * 1. `WP_DEVELOP_DIR` environment variable, which points to a checkout
- *   of the develop.svn.wordpress.org repository (this is recommended)
- * 2. `WP_TESTS_DIR` environment variable, which points to a checkout
- * 3. `WP_ROOT_DIR` environment variable, which points to a checkout
- * 4. Plugin installed inside of WordPress.org developer checkout
- * 5. Tests checked out to /tmp
- */
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
-} elseif ( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	$test_root = getenv( 'WP_TESTS_DIR' );
-} elseif ( false !== getenv( 'WP_ROOT_DIR' ) ) {
-	$test_root = getenv( 'WP_ROOT_DIR' ) . '/tests/phpunit';
-} elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
-	$test_root = '../../../../tests/phpunit';
-} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-	$test_root = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
 
 if ( ! defined( 'BP_TESTS_DIR' ) ) {
 	define( 'BP_TESTS_DIR', dirname( __FILE__ ) . '/../../buddypress/tests/phpunit' );
 }
-
-// Give access to tests_add_filter() function.
-require_once $test_root . '/includes/functions.php';
 
 /**
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
 	// Make sure BP is installed and loaded first.
-	require_once BP_TESTS_DIR . '/includes/loader.php';
+	require BP_TESTS_DIR . '/includes/loader.php';
 
 	// Load our plugin.
 	require_once dirname( __FILE__ ) . '/../bp-rest.php';
@@ -48,7 +36,7 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
-require_once $test_root . '/includes/bootstrap.php';
+require_once $_tests_dir . '/includes/bootstrap.php';
 
-// Load the BP test files.
+echo "Loading BuddyPress testcase...\n";
 require_once BP_TESTS_DIR . '/includes/testcase.php';
