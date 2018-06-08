43f5fb6052244ce3fa93985d8c803799d00dacd3
baconbro <frod666@hotmail.com>
2018-06-08 12:31:41 -0400

    0.1

--- includes/class-bp-rest-notifications-endpoint.php
+++ includes/class-bp-rest-notifications-endpoint.php
@@ -1,1 +1,300 @@
-
+<?php
+/**
+ * BP REST: BP_REST_Notifications_Endpoint class
+ *
+ * @package BuddyPress
+ * @since 0.1.0
+ */
+defined( 'ABSPATH' ) || exit;
+/**
+ * Notifications endpoints.
+ *
+ * @since 0.1.0
+ */
+class BP_REST_Notifications_Endpoint extends WP_REST_Controller {
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.1.0
+	 */
+	public function __construct() {
+		$this->namespace = 'buddypress/v1';
+		$this->rest_base = buddypress()->notifications->id;
+	}
+	/**
+	 * Register the component routes.
+	 *
+	 * @since 0.1.0
+	 */
+	public function register_routes() {
+		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this, 'get_items' ),
+				'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				'args'                => $this->get_collection_params(),
+			),
+			'schema' => array( $this, 'get_item_schema' ),
+		) );
+	}
+	/**
+	 * Retrieve threads.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Request Thread object data.
+	 */
+	public function get_items( $request ) {
+		$args = array(
+			'user_id'      => $request['user_id'],
+			'box'          => $request['box'],
+			'type'         => $request['type'],
+			'page'         => $request['page'],
+			'per_page'     => $request['per_page'],
+			'search_terms' => $request['search'],
+		);
+		$notifications_box = new BP_Notifications_Box_Template( $args );
+		$retval = array();
+		foreach ( $notifications_box->threads as $thread ) {
+			$retval[] = $this->prepare_response_for_collection(
+				$this->prepare_item_for_response( $thread, $request )
+			);
+		}
+		$retval = rest_ensure_response( $retval );
+		/**
+		 * Fires after a thread is fetched via the REST API.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param object           $notifications_box Fetched thread.
+		 * @param WP_REST_Response $retval       The response data.
+		 * @param WP_REST_Request  $request      The request sent to the API.
+		 */
+		do_action( 'rest_notifications_get_items', $notifications_box, $retval, $request );
+		return $retval;
+	}
+	/**
+	 * Check if a given request has access to thread items.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|bool
+	 */
+	public function get_items_permissions_check( $request ) {
+		// Bail early.
+		if ( ! $this->can_see() ) {
+			return new WP_Error( 'rest_user_cannot_view_notifications',
+				__( 'Sorry, you cannot view the notifications.', 'buddypress' ),
+				array(
+					'status' => 500,
+				)
+			);
+		}
+		return true;
+	}
+	/**
+	 * Prepares thread data for return as an object.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param stdClass        $thread Thread data.
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Response
+	 */
+	public function prepare_item_for_response( $thread, $request ) {
+		$data = array(
+			'id'                    => $thread->thread_id,
+			'prime_association'     => $thread->last_notification_id,
+			'secondary_association' => $thread->last_sender_id,
+			'subject'               => $thread->last_notification_subject,
+			'notification'               => $thread->last_notification_content,
+			'date'                  => $thread->last_notification_date,
+			'unread'                => ! empty( $thread->unread_count ) ? $thread->unread_count : 0,
+			'sender_ids'            => $thread->sender_ids,
+			'notifications'              => $thread->notifications,
+		);
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data = $this->add_additional_fields_to_object( $data, $request );
+		$data = $this->filter_response_by_context( $data, $context );
+		$response = rest_ensure_response( $data );
+		$response->add_links( $this->prepare_links( $activity ) );
+		/**
+		 * Filter a thread value returned from the API.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param array           $response
+		 * @param WP_REST_Request $request Request used to generate the response.
+		 */
+		return apply_filters( 'rest_prepare_buddypress_notification_value', $response, $request );
+	}
+	/**
+	 * Prepare links for the request.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $thread Thread.
+	 * @return array Links for the given plugin.
+	 */
+	protected function prepare_links( $thread ) {
+		$base = sprintf( '/%s/%s/', $this->namespace, $this->rest_base );
+		$url  = $base . $thread->thread_id;
+		// Entity meta.
+		$links = array(
+			'self'       => array(
+				'href' => rest_url( $url ),
+			),
+			'collection' => array(
+				'href' => rest_url( $base ),
+			),
+			'user'       => array(
+				'href' => rest_url( '/wp/v2/users/' . $thread->last_sender_id ),
+			),
+		);
+		return $links;
+	}
+	/**
+	 * Can this user see the notification?
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param int $thread_id Thread ID.
+	 * @return boolean
+	 */
+	protected function can_see( $thread_id = 0 ) {
+		$user_id = bp_loggedin_user_id();
+		$retval  = false;
+		// Moderators as well.
+		if ( bp_current_user_can( 'bp_moderate' ) ) {
+			$retval = true;
+		}
+		// Check thread access.
+		if ( ! empty( $thread_id ) && notifications_check_thread_access( $thread_id, $user_id ) ) {
+			$retval = true;
+		}
+		/**
+		 * Filter the retval.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param bool   $retval Return value.
+		 * @param int    $user_id User ID.
+		 */
+		return apply_filters( 'rest_notification_endpoint_can_see', $retval, $user_id );
+	}
+	/**
+	 * Get the plugin schema, conforming to JSON Schema.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'thread',
+			'type'       => 'object',
+			'properties' => array(
+				'id'                    => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'A unique alphanumeric ID for the object.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'integer',
+				),
+				'prime_association'     => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The ID of some other object primarily associated with this one.', 'buddypress' ),
+					'type'        => 'integer',
+				),
+				'subject'               => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'HTML title of the object.', 'buddypress' ),
+					'type'        => 'string',
+				),
+				'notification'               => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'HTML content of the object.', 'buddypress' ),
+					'type'        => 'string',
+				),
+				'date'                  => array(
+					'description' => __( "The date the object was published, in the site's timezone.", 'buddypress' ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+					'context'     => array( 'view', 'edit' ),
+				),
+				'secondary_association' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The ID of some other object also associated with this one.', 'buddypress' ),
+					'type'        => 'integer',
+				),
+				'unread'                => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'The ID of some other object also associated with this one.', 'buddypress' ),
+					'type'        => 'integer',
+				),
+				'notifications'              => array(
+					'description' => __( 'Childrens of the object.', 'buddypress' ),
+					'type'        => 'array',
+					'context'     => array( 'view', 'edit' ),
+				),
+			),
+		);
+		return $schema;
+	}
+	/**
+	 * Get the query params for collections of plugins.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	public function get_collection_params() {
+		$params = parent::get_collection_params();
+		$params['context']['default'] = 'view';
+		$params['box'] = array(
+			'description'       => __( 'Filter the result by box.', 'buddypress' ),
+			'type'              => 'string',
+			'default'           => 'sentbox',
+			'enum'              => array( 'notices', 'sentbox', 'inbox' ),
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['type'] = array(
+			'description'       => __( 'Filter the result by thread status.', 'buddypress' ),
+			'type'              => 'string',
+			'default'           => 'all',
+			'enum'              => array( 'all', 'read', 'unread' ),
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['page'] = array(
+			'description'       => __( 'Offset the result set by a specific number of pages of results.', 'buddypress' ),
+			'default'           => 1,
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['per_page'] = array(
+			'description'       => __( 'Maximum number of results returned per result set.', 'buddypress' ),
+			'default'           => 20,
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['search'] = array(
+			'description'       => __( 'Limit result set to items that match this search query.', 'buddypress' ),
+			'default'           => '',
+			'type'              => 'string',
+			'sanitize_callback' => 'sanitize_text_field',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['user_id'] = array(
+			'description'       => __( 'Limit result to notifications created by specific users.', 'buddypress' ),
+			'type'              => 'integer',
+			'default'           => bp_loggedin_user_id(),
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		return $params;
+	}
+}
