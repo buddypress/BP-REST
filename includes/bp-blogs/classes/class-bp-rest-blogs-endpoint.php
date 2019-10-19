7d7e997477f25ac8274bba2d1276722b54fab2ff
Renato Alves <espellcaste@gmail.com>
2019-10-19 18:47:07 -0300

    Blogs endpoint

---
+++ includes/bp-blogs/classes/class-bp-rest-blogs-endpoint.php
@@ -0,0 +1,350 @@
+<?php
+/**
+ * BP REST: BP_REST_Blogs_Endpoint class
+ *
+ * @package BuddyPress
+ * @since 6.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Blogs endpoints.
+ *
+ * /blogs/
+ *
+ * @since 6.0.0
+ */
+class BP_REST_Blogs_Endpoint extends WP_REST_Controller {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 6.0.0
+	 */
+	public function __construct() {
+		$this->namespace = bp_rest_namespace() . '/' . bp_rest_version();
+		$this->rest_base = buddypress()->blogs->id;
+	}
+
+	/**
+	 * Register the component routes.
+	 *
+	 * @since 6.0.0
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base,
+			array(
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_items' ),
+					'permission_callback' => array( $this, 'get_items_permissions_check' ),
+					'args'                => $this->get_collection_params(),
+				),
+				'schema' => array( $this, 'get_item_schema' ),
+			)
+		);
+	}
+
+	/**
+	 * Retrieve Blogs.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Response
+	 */
+	public function get_items( $request ) {
+		$args = array(
+			'type'             => $request['type'],
+			'include_blog_ids' => $request['include'],
+			'user_id'          => $request['user_id'],
+			'search_terms'     => $request['search'],
+			'page'             => $request['page'],
+			'per_page'         => $request['per_page'],
+		);
+
+		/**
+		 * Filter the query arguments for the request.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param array           $args    Key value array of query var to query value.
+		 * @param WP_REST_Request $request The request sent to the API.
+		 */
+		$args = apply_filters( 'bp_rest_blogs_get_items_query_args', $args, $request );
+
+		// false is the default value for some args.
+		foreach ( $args as $key => $value ) {
+			if ( empty( $value ) ) {
+				$args[ $key ] = false;
+			}
+		}
+
+		// Check if user is valid.
+		if ( ! empty( $request['user_id'] ) ) {
+			$user = get_user_by( 'id', $request['user_id'] );
+			if ( ! $user instanceof WP_User ) {
+				return new WP_Error(
+					'bp_rest_blogs_get_items_user_failed',
+					__( 'There was a problem confirming if user ID provided is a valid one.', 'buddypress' ),
+					array(
+						'status' => 500,
+					)
+				);
+			}
+		}
+
+		// Actually, query it.
+		$blogs = bp_blogs_get_blogs( $args );
+
+		$retval = array();
+		foreach ( (array) $blogs['blogs'] as $blog ) {
+			$retval[] = $this->prepare_response_for_collection(
+				$this->prepare_item_for_response( $blog, $request )
+			);
+		}
+
+		$response = rest_ensure_response( $retval );
+		$response = bp_rest_response_add_total_headers( $response, $blogs['total'], $args['per_page'] );
+
+		/**
+		 * Fires after blogs are fetched via the REST API.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param array            $blogs     Fetched blogs.
+		 * @param WP_REST_Response $response  The response data.
+		 * @param WP_REST_Request  $request   The request sent to the API.
+		 */
+		do_action( 'bp_rest_blogs_get_items', $blogs, $response, $request );
+
+		return $response;
+	}
+
+	/**
+	 * Check if a given request has access to blog items.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|bool
+	 */
+	public function get_items_permissions_check( $request ) {
+		$retval = true;
+
+		if ( ! is_user_logged_in() ) {
+			$retval = new WP_Error(
+				'bp_rest_authorization_required',
+				__( 'Sorry, you need to be logged in to perform this action.', 'buddypress' ),
+				array(
+					'status' => rest_authorization_required_code(),
+				)
+			);
+		}
+
+		/**
+		 * Filter the blogs `get_items` permissions check.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param bool|WP_Error   $retval  Returned value.
+		 * @param WP_REST_Request $request The request sent to the API.
+		 */
+		return apply_filters( 'bp_rest_blogs_get_items_permissions_check', $retval, $request );
+	}
+
+	/**
+	 * Prepares blogs data for return as an object.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param BP_Blogs_Blog   $blog    Blog object.
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Response
+	 */
+	public function prepare_item_for_response( $blog, $request ) {
+		$data = array(
+			'id'            => $blog->blog_id,
+			'user_id'       => $blog->admin_user_id,
+			'name'          => $blog->name,
+			'domain'        => $blog->domain,
+			'path'          => $blog->path,
+			'description'   => $blog->description,
+			'last_activity' => $blog->last_activity,
+		);
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		$response = rest_ensure_response( $data );
+		$response->add_links( $this->prepare_links( $blog ) );
+
+		/**
+		 * Filter a blog returned from the API.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param WP_REST_Response  $response Response generated by the request.
+		 * @param WP_REST_Request   $request  Request used to generate the response.
+		 * @param BP_Blogs_Blog     $blog     The blog object.
+		 */
+		return apply_filters( 'bp_rest_blogs_prepare_value', $response, $request, $blog );
+	}
+
+	/**
+	 * Prepare links for the request.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param BP_Blogs_Blog $blog Blog object.
+	 * @return array
+	 */
+	protected function prepare_links( $blog ) {
+		$base = sprintf( '/%s/%s/', $this->namespace, $this->rest_base );
+		$url  = $base . $blog->blog_id;
+
+		// Entity meta.
+		$links = array(
+			'self'       => array(
+				'href' => rest_url( $url ),
+			),
+			'collection' => array(
+				'href' => rest_url( $base ),
+			),
+			'user'       => array(
+				'href'       => rest_url( bp_rest_get_user_url( $blog->admin_user_id ) ),
+				'embeddable' => true,
+			),
+		);
+
+		/**
+		 * Filter links prepared for the REST response.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param array         $links The prepared links of the REST response.
+		 * @param BP_Blogs_Blog $blog  Blog object.
+		 */
+		return apply_filters( 'bp_rest_blogs_prepare_links', $links, $blog );
+	}
+
+	/**
+	 * Get the blogs schema, conforming to JSON Schema.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'bp_blogs',
+			'type'       => 'object',
+			'properties' => array(
+				'id'           => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'A unique numeric ID for the blog.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'integer',
+				),
+				'user_id' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'A unique numeric ID for blog admin.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'integer',
+				),
+				'name' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Name of the blog.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'string',
+					'arg_options' => array(
+						'sanitize_callback' => 'sanitize_text_field',
+					),
+				),
+				'description' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Description of the blog.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'string',
+				),
+				'path' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Path of the blog.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'string',
+				),
+				'domain' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Domain of the blog.', 'buddypress' ),
+					'readonly'    => true,
+					'type'        => 'string',
+				),
+				'last_activity' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( "The last activity date from the blog, in the site's timezone.", 'buddypress' ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+				),
+			),
+		);
+
+		/**
+		 * Filter the blogs schema.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param array $schema The endpoint schema.
+		 */
+		return apply_filters( 'bp_rest_blogs_schema', $this->add_additional_fields_schema( $schema ) );
+	}
+
+	/**
+	 * Get the query params for blogs collections.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @return array
+	 */
+	public function get_collection_params() {
+		$params                       = parent::get_collection_params();
+		$params['context']['default'] = 'view';
+
+		$params['user_id'] = array(
+			'description'       => __( 'ID of the user whose blogs user can post to.', 'buddypress' ),
+			'default'           => 0,
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['include'] = array(
+			'description'       => __( 'Ensure result set includes specific IDs.', 'buddypress' ),
+			'default'           => array(),
+			'type'              => 'array',
+			'items'             => array( 'type' => 'integer' ),
+			'sanitize_callback' => 'wp_parse_id_list',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['type'] = array(
+			'description'       => __( 'Limit result set to items with a specific type.', 'buddypress' ),
+			'default'           => 'active',
+			'type'              => 'string',
+			'enum'              => array( 'active', 'alphabetical', 'newest', 'random' ),
+			'sanitize_callback' => 'sanitize_key',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		/**
+		 * Filters the collection query params.
+		 *
+		 * @param array $params Query params.
+		 */
+		return apply_filters( 'bp_rest_blogs_collection_params', $params );
+	}
+}
---
+++ tests/blogs/test-controller.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Blogs Endpoint Tests.
+ *
+ * @package BuddyPress
+ * @subpackage BP_REST
+ * @group blogs
+ */
+class BP_Test_REST_Blogs_Endpoint extends WP_Test_REST_Controller_Testcase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->bp_factory   = new BP_UnitTest_Factory();
+		$this->endpoint     = new BP_REST_Blogs_Endpoint();
+		$this->bp           = new BP_UnitTestCase();
+		$this->endpoint_url = '/' . bp_rest_namespace() . '/' . bp_rest_version() . '/' . buddypress()->blogs->id;
+
+		if ( ! $this->server ) {
+			$this->server = rest_get_server();
+		}
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function test_register_routes() {
+		$routes = $this->server->get_routes();
+
+		// Main.
+		$this->assertArrayHasKey( $this->endpoint_url, $routes );
+		$this->assertCount( 1, $routes[ $this->endpoint_url ] );
+	}
+
+	/**
+	 * @group get_items
+	 */
+	public function test_get_items() {
+		if ( ! is_multisite() ) {
+			$this->markTestSkipped();
+		}
+
+		if ( function_exists( 'wp_initialize_site' ) ) {
+			$this->setExpectedDeprecated( 'wpmu_new_blog' );
+		}
+
+		$u = $this->bp_factory->user->create();
+
+		$this->bp->set_current_user( $u );
+
+		$b = $this->bp_factory->blog->create();
+
+		$request = new WP_REST_Request( 'GET', $this->endpoint_url );
+		$request->set_param( 'context', 'view' );
+		$response = $this->server->dispatch( $request );
+
+		$this->assertEquals( 200, $response->get_status() );
+
+		$blogs = $response->get_data();
+
+		$headers = $response->get_headers();
+
+		$this->assertEquals( 1, $headers['X-WP-Total'] );
+		$this->assertEquals( 1, $headers['X-WP-TotalPages'] );
+
+		$this->assertTrue( ! empty( $blogs[0] ) );
+		$this->assertSame( $blogs[0]['user_id'], $u );
+	}
+
+	/**
+	 * @group get_items
+	 */
+	public function test_get_items_user_not_logged_in() {
+		$request = new WP_REST_Request( 'GET', $this->endpoint_url );
+		$request->set_param( 'context', 'view' );
+		$response = $this->server->dispatch( $request );
+
+		$this->assertErrorResponse( 'bp_rest_authorization_required', $response, rest_authorization_required_code() );
+	}
+
+	/**
+	 * @group get_item
+	 */
+	public function test_get_item() {
+		return true;
+	}
+
+	/**
+	 * @group create_item
+	 */
+	public function test_create_item() {
+		return true;
+	}
+
+	/**
+	 * @group update_item
+	 */
+	public function test_update_item() {
+		return true;
+	}
+
+	/**
+	 * @group delete_item
+	 */
+	public function test_delete_item() {
+		return true;
+	}
+
+	public function test_prepare_item() {
+		return true;
+	}
+
+	protected function check_blog_data( $blog, $data ) {
+		$this->assertEquals( $blog->blog_id, $data['id'] );
+		$this->assertEquals( $blog->user_id, $data['user_id'] );
+		$this->assertEquals( $blog->name, $data['name'] );
+		$this->assertEquals( $blog->domain, $data['domain'] );
+		$this->assertEquals( $blog->path, $data['path'] );
+		$this->assertEquals( $blog->description, $data['description'] );
+		$this->assertEquals( bp_rest_prepare_date_response( $blog->last_activity ), $data['last_activity'] );
+	}
+
+	public function test_get_item_schema() {
+		$request    = new WP_REST_Request( 'OPTIONS', $this->endpoint_url );
+		$response   = $this->server->dispatch( $request );
+		$data       = $response->get_data();
+		$properties = $data['schema']['properties'];
+
+		$this->assertEquals( 7, count( $properties ) );
+		$this->assertArrayHasKey( 'id', $properties );
+		$this->assertArrayHasKey( 'user_id', $properties );
+		$this->assertArrayHasKey( 'name', $properties );
+		$this->assertArrayHasKey( 'path', $properties );
+		$this->assertArrayHasKey( 'domain', $properties );
+		$this->assertArrayHasKey( 'description', $properties );
+		$this->assertArrayHasKey( 'last_activity', $properties );
+	}
+
+	public function test_context_param() {
+		$request  = new WP_REST_Request( 'OPTIONS', $this->endpoint_url );
+		$response = $this->server->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'view', $data['endpoints'][0]['args']['context']['default'] );
+		$this->assertEquals( array( 'view', 'edit' ), $data['endpoints'][0]['args']['context']['enum'] );
+	}
+}
