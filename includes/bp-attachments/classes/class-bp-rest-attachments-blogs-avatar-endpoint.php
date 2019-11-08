ac7b3475c343f7d16fb89a76fe48d72bd4ca3f99
Renato Alves <renato@alley.co>
2019-11-07 23:29:43 -0300

    Adding basic blog avatar support

---
+++ includes/bp-attachments/classes/class-bp-rest-attachments-blogs-avatar-endpoint.php
@@ -0,0 +1,274 @@
+<?php
+/**
+ * BP REST: BP_REST_Attachments_Blogs_Avatar_Endpoint class
+ *
+ * @package BuddyPress
+ * @since 6.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * blog avatar endpoints.
+ *
+ * @since 6.0.0
+ */
+class BP_REST_Attachments_Blogs_Avatar_Endpoint extends WP_REST_Controller {
+
+    use BP_REST_Attachments;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 6.0.0
+	 */
+	public function __construct() {
+		$this->namespace = bp_rest_namespace() . '/' . bp_rest_version();
+		$this->rest_base = buddypress()->blogs->id;
+	}
+
+	/**
+	 * Register the component routes.
+	 *
+	 * @since 6.0.0
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/(?P<blog_id>[\d]+)/avatar',
+			array(
+				'args'   => array(
+					'blog_id' => array(
+						'description' => __( 'A unique numeric ID for the blog.', 'buddypress' ),
+						'type'        => 'integer',
+					),
+				),
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_item' ),
+					'permission_callback' => array( $this, 'get_item_permissions_check' ),
+					'args'                => $this->get_item_collection_params(),
+				),
+				'schema' => array( $this, 'get_item_schema' ),
+			)
+		);
+	}
+
+	/**
+	 * Fetch an existing blog avatar.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Response|WP_Error
+	 */
+	public function get_item( $request ) {
+        $args = array();
+
+        foreach ( array( 'full', 'thumb' ) as $type ) {
+			$args[ $type ] = bp_get_blog_avatar(
+				array(
+					'type'    => $type,
+                    'blog_id' => $request['blog_id'],
+                    'alt'     => $request['alt'],
+                    'no_grav' => (bool) $request['no_grav'],
+				)
+			);
+        }
+        
+        // Get the avatar object.
+		$avatar = $this->get_avatar_object( $args );
+
+		if ( ! $avatar->full && ! $avatar->thumb ) {
+			return new WP_Error(
+				'bp_rest_attachments_blogs_avatar_no_image',
+				__( 'Sorry, there was a problem fetching the blog avatar.', 'buddypress' ),
+				array(
+					'status' => 500,
+				)
+			);
+		}
+
+		$retval = array(
+			$this->prepare_response_for_collection(
+				$this->prepare_item_for_response( $avatar, $request )
+			),
+		);
+
+		$response = rest_ensure_response( $retval );
+
+		/**
+		 * Fires after a blog avatar is fetched via the REST API.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param string            $avatar   The avatar.
+		 * @param WP_REST_Response  $response The response data.
+		 * @param WP_REST_Request   $request  The request sent to the API.
+		 */
+		do_action( 'bp_rest_attachments_blogs_avatar_get_item', $avatar, $response, $request );
+
+		return $response;
+	}
+
+	/**
+	 * Checks if a given request has access to get a blog avatar.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return bool|WP_Error
+	 */
+	public function get_item_permissions_check( $request ) {
+		$retval = true;
+		$blogs  = bp_blogs_get_blogs(
+            array(
+                'include_blog_ids'  => $request['blog_id'],
+                'per_page'          => 1,
+                'update_meta_cache' => false,
+            )
+        );
+
+        $blog = (array) $blogs['blogs'][0];
+
+		if ( true === $retval && ! $blog instanceof BP_Blogs_Blog ) {
+			$retval = new WP_Error(
+				'bp_rest_blog_invalid_id',
+				__( 'Invalid group ID.', 'buddypress' ),
+				array(
+					'status' => 404,
+				)
+			);
+        }
+        
+        if ( true === $retval && ! buddypress()->avatar->show_avatars ) {
+			$retval = new WP_Error(
+				'bp_rest_attachments_blogs_avatar_disabled',
+				__( 'Sorry, blog avatar upload is disabled.', 'buddypress' ),
+				array(
+					'status' => 500,
+				)
+			);
+		}
+
+		/**
+		 * Filter the blog avatar `get_item` permissions check.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param bool|WP_Error   $retval  Returned value.
+		 * @param WP_REST_Request $request The request sent to the API.
+		 */
+		return apply_filters( 'bp_rest_attachments_blogs_avatar_get_item_permissions_check', $retval, $request );
+	}
+
+	/**
+	 * Prepares avatar data to return as an object.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @param object          $avatar  Avatar object.
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return WP_REST_Response
+	 */
+	public function prepare_item_for_response( $avatar, $request ) {
+		$data = array(
+			'full'  => $avatar->full,
+			'thumb' => $avatar->thumb,
+		);
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		// @todo add prepare_links
+		$response = rest_ensure_response( $data );
+
+		/**
+		 * Filter a blog avatar value returned from the API.
+		 *
+		 * @since 6.0.0
+		 *
+		 * @param WP_REST_Response  $response Response.
+		 * @param WP_REST_Request   $request  Request used to generate the response.
+		 * @param object            $avatar   Avatar object.
+		 */
+		return apply_filters( 'bp_rest_attachments_blogs_avatar_prepare_value', $response, $request, $avatar );
+	}
+
+	/**
+	 * Get the blog avatar schema, conforming to JSON Schema.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'bp_attachments_blog_avatar',
+			'type'       => 'object',
+			'properties' => array(
+				'full'  => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Full size of the image file.', 'buddypress' ),
+					'type'        => 'string',
+					'format'      => 'uri',
+					'readonly'    => true,
+				),
+				'thumb' => array(
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Thumb size of the image file.', 'buddypress' ),
+					'type'        => 'string',
+					'format'      => 'uri',
+					'readonly'    => true,
+				),
+			),
+		);
+
+		/**
+		 * Filters the blog avatar schema.
+		 *
+		 * @param string $schema The endpoint schema.
+		 */
+		return apply_filters( 'bp_rest_attachments_blogs_avatar_schema', $this->add_additional_fields_schema( $schema ) );
+	}
+
+	/**
+	 * Get the query params for the `get_item`.
+	 *
+	 * @since 6.0.0
+	 *
+	 * @return array
+	 */
+	public function get_item_collection_params() {
+		$params                       = parent::get_collection_params();
+		$params['context']['default'] = 'view';
+
+		// Removing unused params.
+		unset( $params['search'], $params['page'], $params['per_page'] );
+
+		$params['alt'] = array(
+			'description'       => __( 'The alt attribute for the <img> element.', 'buddypress' ),
+			'default'           => '',
+			'type'              => 'string',
+			'sanitize_callback' => 'sanitize_text_field',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		$params['no_grav'] = array(
+			'description'       => __( 'Whether to disable the default Gravatar fallback.', 'buddypress' ),
+			'default'           => false,
+			'type'              => 'boolean',
+			'sanitize_callback' => 'rest_sanitize_boolean',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+
+		/**
+		 * Filters the item collection query params.
+		 *
+		 * @param array $params Query params.
+		 */
+		return apply_filters( 'bp_rest_attachments_blogs_avatar_collection_params', $params );
+	}
+}
---
+++ tests/attachments/test-blog-avatar-controller.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Blog Avatar Endpoints Tests.
+ *
+ * @package BuddyPress
+ * @subpackage BP_REST
+ * @group blog-avatar
+ */
+class BP_Test_REST_Attachments_Blog_Avatar_Endpoint extends WP_Test_REST_Controller_Testcase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->bp_factory   = new BP_UnitTest_Factory();
+		$this->endpoint     = new BP_REST_Attachments_Blogs_Avatar_Endpoint();
+		$this->bp           = new BP_UnitTestCase();
+        $this->endpoint_url = '/' . bp_rest_namespace() . '/' . bp_rest_version() . '/' . buddypress()->blogs->id . '/';
+
+		if ( ! $this->server ) {
+			$this->server = rest_get_server();
+		}
+
+		$this->old_current_user = get_current_user_id();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function test_register_routes() {
+		$routes   = $this->server->get_routes();
+		$endpoint = $this->endpoint_url . '(?P<blog_id>[\d]+)/avatar';
+
+		// Single.
+		$this->assertArrayHasKey( $endpoint, $routes );
+		$this->assertCount( 1, $routes[ $endpoint ] );
+	}
+
+	/**
+	 * @group get_items
+	 */
+	public function test_get_items() {
+		return true;
+	}
+
+	/**
+	 * @group get_item
+	 */
+	public function test_get_item() {
+        if ( ! is_multisite() ) {
+			$this->markTestSkipped();
+		}
+
+		if ( function_exists( 'wp_initialize_site' ) ) {
+			$this->setExpectedDeprecated( 'wpmu_new_blog' );
+        }
+        
+        $blog_id = $this->bp_factory->blog->create();
+
+		$request = new WP_REST_Request( 'GET', sprintf( $this->endpoint_url . '%d/avatar', $blog_id ) );
+		$request->set_param( 'context', 'view' );
+		$response = rest_get_server()->dispatch( $request );
+
+		$this->assertEquals( 200, $response->get_status() );
+
+		$all_data = $response->get_data();
+		$this->assertNotEmpty( $all_data );
+
+		$this->assertTrue( isset( $all_data[0]['full'] ) && isset( $all_data[0]['thumb'] ) );
+	}
+
+	/**
+	 * @group get_item
+	 */
+	public function test_get_item_invalid_blog_id() {
+        if ( ! is_multisite() ) {
+			$this->markTestSkipped();
+		}
+
+		if ( function_exists( 'wp_initialize_site' ) ) {
+			$this->setExpectedDeprecated( 'wpmu_new_blog' );
+        }
+
+		$request  = new WP_REST_Request( 'GET', sprintf( $this->endpoint_url . '%d/avatar', REST_TESTS_IMPOSSIBLY_HIGH_NUMBER ) );
+		$response = rest_get_server()->dispatch( $request );
+		$this->assertErrorResponse( 'bp_rest_blog_invalid_id', $response, 404 );
+	}
+
+	/**
+	 * @group create_item
+	 */
+	public function test_create_item() {
+		return true;
+	}
+
+	/**
+	 * @group update_item
+	 */
+	public function test_update_item() {
+		return true;
+	}
+
+	/**
+	 * @group delete_item
+	 */
+	public function test_delete_item() {
+		return true;
+	}
+
+	/**
+	 * @group prepare_item
+	 */
+	public function test_prepare_item() {
+		return true;
+	}
+
+	public function test_get_item_schema() {
+        if ( ! is_multisite() ) {
+			$this->markTestSkipped();
+		}
+
+		if ( function_exists( 'wp_initialize_site' ) ) {
+			$this->setExpectedDeprecated( 'wpmu_new_blog' );
+        }
+
+        $blog_id = $this->bp_factory->blog->create();
+
+        // Single.
+		$request    = new WP_REST_Request( 'OPTIONS', sprintf( $this->endpoint_url . '%d/avatar', $blog_id ) );
+		$response   = $this->server->dispatch( $request );
+		$data       = $response->get_data();
+		$properties = $data['schema']['properties'];
+
+		$this->assertEquals( 2, count( $properties ) );
+		$this->assertArrayHasKey( 'full', $properties );
+		$this->assertArrayHasKey( 'thumb', $properties );
+	}
+
+	public function test_context_param() {
+        if ( ! is_multisite() ) {
+			$this->markTestSkipped();
+		}
+
+		if ( function_exists( 'wp_initialize_site' ) ) {
+			$this->setExpectedDeprecated( 'wpmu_new_blog' );
+        }
+
+        $blog_id = $this->bp_factory->blog->create();
+
+		// Single.
+		$request  = new WP_REST_Request( 'OPTIONS', sprintf( $this->endpoint_url . '%d/avatar', $blog_id ) );
+		$response = $this->server->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertNotEmpty( $data );
+	}
+}
